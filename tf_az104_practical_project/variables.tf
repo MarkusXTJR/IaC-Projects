d5086a8f690a67e3533723e093aaf7fe12439d48
Ryan Jackson <138688223+MarkusXTJR@users.noreply.github.com>
2023-11-20 00:28:47 +0000

    statred az104 project, setup basic virtual machine

--- tf_az104_practical_project/variables.tf
+++ tf_az104_practical_project/variables.tf
@@ -2,21 +2,21 @@ variable "hv_host_username" {
     description = "Username for hypervisor 01"
     type        = string
     sensitive   = true
-    default     = "adminuser"  #Declare here for ease #IMPORTANT! always delete before push to Github
+    default     = ""  #Declare here for ease #IMPORTANT! always delete before push to Github
 }
 
 variable "hv_host_password" {
     description = "password for hypervisor 01"
     type        = string
     sensitive   = true
-    default = "#L3v1S#wnd0n" #Declare here for ease #IMPORTANT! always delete before push to Github
+    default = "" #Declare here for ease #IMPORTANT! always delete before push to Github
 }
 
 variable "mypublicip" {
     description = "Public IP for the VM"
     type        = string
     sensitive   = true
-    default     = "109.158.196.196" #Declare here for ease #IMPORTANT! always delete before push to Github
+    default     = "" #Declare here for ease #IMPORTANT! always delete before push to Github
 }
 
 variable "tags" {
